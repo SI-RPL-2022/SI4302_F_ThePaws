fd2c65248c703f7bf5260f44dc14d02031ffceb6
jaegerjaques08 <harveyalamudi@gmail.com>
2022-04-06 18:55:09 +0800

    Membuat login as admin ref #5

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     You are Admin.
+                    
                 </div>
             </div>
         </div>
