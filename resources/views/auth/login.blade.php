e16fe7f06a95b6c88bc5dea46e7f33647ebc02e1
ramadwif <ramadwifadillah@gmail.com>
2022-04-07 20:59:03 +0700

    fix front end login dan register ref #5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container"> 
+<div class="container">
     <div class="row align-items-center" >
         <div class="col-12 col-md-12 col-lg-4">
             <div class="d-flex align-items-center">
                 <a href="{{ url('/') }}"> <img src="img/logo2.png" class="img-fluid" width="100%" alt="pawsregister"></a>
-            </div>            
+            </div>
         </div>
         <div class="col-12 col-md-12 col-lg-8 mh-100" style="border-left:2px solid #8DB1F3; height:670px; border-radius: 20px;" ><br>
             <h3 class="fs-4 text-center" style="margin-bottom: 80px; "><b>Login</b></h3>
@@ -14,17 +14,13 @@
             <div class="row justify-content-center">
                 <div class="col-12 col-md-12 col-lg-6" >
 
-                <div class="mt-2 text-center" style="margin-bottom: 20px">
-                    <a class="btn btn-sm btn-outline-primary" href="#" style="font-size:12px;">&nbsp Login as Admin</a>
-                </div>
-
                     <form method="POST" action="{{ route('login') }}">
-                    
+
                          @csrf
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="email" style="border-bottom:3px solid #8DB1F3" name="email" required autocomplete="email" class="form-control form-control-sm  border-top-0 border-end-0 border-start-0 @error('email') is-invalid @enderror" id="floatingEmail" placeholder="Email"  value="{{ old('email') }}">
-                                    <label for="floatingEmail">{{ __('Email') }}</label> 
-                                    
+                                    <label for="floatingEmail">{{ __('Email') }}</label>
+
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -35,7 +31,7 @@
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="password" style="border-bottom:3px solid #8DB1F3" name="password" required autocomplete="new-password" class="form-control form-control-sm border-top-0 border-end-0 border-start-0 @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password">
                                     <label for="floatingPassword">{{ __('Password') }}</label>
-                                    
+
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -68,3 +64,4 @@
     </div>
 </div>
 @endsection
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container"> 
+<div class="container">
     <div class="row align-items-center" >
         <div class="col-12 col-md-12 col-lg-4">
             <div class="d-flex align-items-center">
                 <a href="{{ url('/') }}"> <img src="img/logo2.png" class="img-fluid" width="100%" alt="pawsregister"></a>
-            </div>            
+            </div>
         </div>
         <div class="col-12 col-md-12 col-lg-8 mh-100" style="border-left:2px solid #8DB1F3; height:675px; border-radius: 20px;" ><br>
             <h3 class="fs-4 text-center" style="margin-bottom: 90px; "><b>Create Account</b></h3>
-            
+
             <div class="row justify-content-center">
                 <div class="col-12 col-md-12 col-lg-6" >
                     <form method="POST" action="{{ route('register') }}">
@@ -19,7 +19,7 @@
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="text" style="border-bottom:3px solid #8DB1F3" name="name" required autocomplete="name" class="form-control form-control-sm border-top-0 border-end-0 border-start-0 @error('name') is-invalid @enderror" id="floatingName" placeholder="Full Name" value="{{ old('name') }}" autofocus>
                                     <label for="floatingName">{{ __('Full Name') }}</label>
-                                     
+
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -29,7 +29,7 @@
 
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="email" style="border-bottom:3px solid #8DB1F3" name="email" required autocomplete="email" class="form-control form-control-sm  border-top-0 border-end-0 border-start-0 @error('email') is-invalid @enderror" id="floatingEmail" placeholder="Email"  value="{{ old('email') }}">
-                                    <label for="floatingEmail">{{ __('Email') }}</label>  
+                                    <label for="floatingEmail">{{ __('Email') }}</label>
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -40,7 +40,7 @@
 
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="password" style="border-bottom:3px solid #8DB1F3" name="password" required autocomplete="new-password" class="form-control form-control-sm border-top-0 border-end-0 border-start-0 @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password">
-                                    <label for="floatingPassword">{{ __('Password') }}</label>  
+                                    <label for="floatingPassword">{{ __('Password') }}</label>
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -51,7 +51,7 @@
 
                             <div class="form-floating mb-2" style="font-size:13px;">
                                 <input type="password" style="border-bottom:3px solid #8DB1F3" name="password_confirmation" required autocomplete="new-password" class="form-control form-control-sm border-top-0 border-end-0 border-start-0 @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Confirm Password">
-                                <label for="floatingPassword">{{ __('Confirm Password') }}</label>  
+                                <label for="floatingPassword">{{ __('Confirm Password') }}</label>
                             </div>
 
                             <div class="d-grid gap-3 col-6 mx-auto">
