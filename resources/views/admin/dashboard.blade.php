42e834260c213b62c33055ae92e3ad6ed348ade9
anandaviamianni <ananda.viamianni212@gmail.com>
2022-04-08 11:10:03 +0700

    rebase from main

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin-layouts.main')
+@section('container')
+
+<div class="container-fluid" style="min-height: 768px; background-color: #F2F2F2;">
+    <div class="row">
+        @include('admin-layouts.sidebar')
+    </div>
+</div>
+@endsection
