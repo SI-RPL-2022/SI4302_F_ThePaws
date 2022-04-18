7e2bd261462a4bb128cd6920dcaec18a21be3f45
anandaviamianni <ananda.viamianni212@gmail.com>
2022-04-18 07:58:34 +0700

    menambahkan item blog seeder

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -17,10 +17,10 @@ class BlogSeeder extends Seeder
         $blog = [
             [
                 'judul' => 'Penyebab Dan Cara Mengobati Kucing Jika Muntah',
-                'kategori1' => '1',
+                'kategori1' => '2',
                 'kategori2' => '2',
                 'creator' => 'Admin',
-                'foto' => 'muntah.jpg',
+                'foto' => 'blog4.png',
                 'deskripsi' => 'Kucing adalah hewan yang cukup sensitif pada bagian pencernaannya. Ada beberapa penyakit yang bisa menyerang kucing melalui pencernaan.
             Salah satu gejala yang bisa menjadi penyebab kucing sakit keras adalah kucing muntah busa. Apakah kamu pernah melihat kondisi seperti ini? Kondisi dimana kucing muntah berbusa dengan intensitas yang cukup sering.
            Segera ketahui beberapa penyebab dan cara mengobati kucing yang sudah mengalami kondisi seperti ini sebelum penyakitnya merambat menjadi yang lebih berat.
@@ -39,11 +39,87 @@ class BlogSeeder extends Seeder
             ],
             [
                 'judul' => 'Cara merawat anak anjing tanpa induk',
-                'kategori1' => '1',
+                'kategori1' => '2',
                 'kategori2' => '2',
                 'creator' => 'Admin',
                 'foto' => 'blog1.png',
-                'deskripsi' => 'blablabla',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Cara Membawa Kucing ke Vet untuk suntik',
+                'kategori1' => '2',
+                'kategori2' => '2',
+                'creator' => 'Admin',
+                'foto' => 'blog2.png',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Penanganan Pertama Adopsi Kucing Liar',
+                'kategori1' => '2',
+                'kategori2' => '1',
+                'creator' => 'Admin',
+                'foto' => 'blog3.png',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Cara Menangani Anjing Sedih',
+                'kategori1' => '1',
+                'kategori2' => '1',
+                'creator' => 'Admin',
+                'foto' => 'blog5.png',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Cara Mengajak Anjing Introvert Jalan-Jalan',
+                'kategori1' => '1',
+                'kategori2' => '3',
+                'creator' => 'Admin',
+                'foto' => 'blog6.png',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Perawatan Anjing Diam',
+                'kategori1' => '1',
+                'kategori2' => '1',
+                'creator' => 'Admin',
+                'foto' => 'Blogs.jpg',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
+            ],
+            [
+                'judul' => 'Cara Mengatasi Kucing Marah',
+                'kategori1' => '2',
+                'kategori2' => '3',
+                'creator' => 'Admin',
+                'foto' => 'kucing marah.jpeg',
+                'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum mi dui. Morbi porttitor tincidunt risus eget aliquet. Nulla non ex hendrerit risus faucibus fermentum ut sed dolor. Donec non vestibulum nunc. Praesent ac mollis orci, sit amet feugiat neque. Cras bibendum diam vel dui vehicula tincidunt et ac libero. Vivamus sit amet tincidunt sapien. Suspendisse suscipit neque non pellentesque maximus. Nunc semper pellentesque libero, vitae lacinia neque vestibulum eget. Etiam et enim fermentum, hendrerit ipsum ac, porta neque. Duis vitae luctus ligula.
+
+                Maecenas condimentum, quam sed varius gravida, ex sem congue risus, ac gravida nisl ipsum vel tellus. Aenean eu volutpat sem, eu vehicula ipsum. Vivamus imperdiet risus et lectus dignissim, id congue lectus accumsan. Aenean nunc nibh, malesuada non pretium sit amet, rutrum a dui. Mauris ligula purus, sodales id tristique in, venenatis eu felis. Donec efficitur augue eu neque malesuada tincidunt. Mauris quis aliquam turpis. Donec tincidunt, ante vel euismod mattis, odio sem consectetur urna, in mattis mauris lacus eget tortor. Nullam non lacinia metus, in fringilla felis.
+                
+                Phasellus vel quam nec nisi gravida pulvinar. Pellentesque nec rhoncus arcu. Vivamus mollis tellus non libero vulputate sagittis. Cras mollis in lacus et condimentum. Phasellus ac enim consequat, rutrum nisi eget, blandit lectus. Sed maximus turpis eu ullamcorper euismod. Integer vitae lorem id purus fermentum commodo at non nunc. Cras euismod vestibulum ante eu consectetur.',
             ],
         ];
 
